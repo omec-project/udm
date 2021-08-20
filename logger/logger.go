0cb5814806c37333c706035439ffa290936ce853
Pingping Lin <pingping@opennetworking.org>
2021-08-20 10:32:24 -0500

    disable logs to file

--- logger/logger.go
+++ logger/logger.go
@@ -1,14 +1,16 @@
+// SPDX-FileCopyrightText: 2021 Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+
 package logger
 
 import (
-	"os"
 	"time"
 
 	formatter "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/sirupsen/logrus"
 
-	"github.com/free5gc/logger_conf"
-	"github.com/free5gc/logger_util"
 )
 
 var (
@@ -42,15 +44,6 @@ func init() {
 		FieldsOrder:     []string{"component", "category"},
 	}
 
-	free5gcLogHook, err := logger_util.NewFileHook(logger_conf.Free5gcLogFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o666)
-	if err == nil {
-		log.Hooks.Add(free5gcLogHook)
-	}
-
-	selfLogHook, err := logger_util.NewFileHook(logger_conf.NfLogDir+"udm.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o666)
-	if err == nil {
-		log.Hooks.Add(selfLogHook)
-	}
 
 	AppLog = log.WithFields(logrus.Fields{"component": "UDM", "category": "App"})
 	InitLog = log.WithFields(logrus.Fields{"component": "UDM", "category": "Init"})
